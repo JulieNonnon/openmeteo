c1ee9069e8e392cd78c622fa7a41d01a37c5e9b9
JulieNonnon <julie.nonnon@gmail.com>
2025-06-18 22:59:08 +0200

    created config.json, updated data.js and index.js, got rid of the search input.

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { MainCard } from "../components/MainCard";
 import { ContentBox } from "../components/ContentBox";
 import { Header } from "../components/Header";
 import { DateAndTime } from "../components/DateAndTime";
-import { Search } from "../components/Search";
+//import { Search } from "../components/Search";
 import { MetricsBox } from "../components/MetricsBox";
 import { UnitSwitch } from "../components/UnitSwitch";
 import { LoadingScreen } from "../components/LoadingScreen";
@@ -13,8 +13,8 @@ import { ErrorScreen } from "../components/ErrorScreen";
 import styles from "../styles/Home.module.css";
 
 export const App = () => {
-  const [cityInput, setCityInput] = useState("Riga");
-  const [triggerFetch, setTriggerFetch] = useState(true);
+  //const [cityInput, setCityInput] = useState("Riga");
+  //const [triggerFetch, setTriggerFetch] = useState(true);
   const [weatherData, setWeatherData] = useState(null);
   const [unitSystem, setUnitSystem] = useState("metric");
   const [error, setError] = useState(null);
@@ -25,10 +25,10 @@ export const App = () => {
         const res = await fetch("/api/data", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ cityInput }),
+          //body: JSON.stringify({ cityInput }),
         });
 
-        if (!res.ok) throw new Error("Erreur lors de la récupération des données");
+        if (!res.ok) throw new Error("Error while getting data");
 
         const data = await res.json();
         setWeatherData(data);
@@ -41,22 +41,25 @@ export const App = () => {
     };
 
     getData();
-  }, [triggerFetch]);
+  //}, [triggerFetch]);
+  }, []); // auto call while loading
 
   const changeSystem = () =>
     setUnitSystem((prev) => (prev === "metric" ? "imperial" : "metric"));
 
-  if (error) {
-    return (
-      <ErrorScreen errorMessage={error}>
-        <Search
-          onFocus={(e) => (e.target.value = "")}
-          onChange={(e) => setCityInput(e.target.value)}
-          onKeyDown={(e) => e.keyCode === 13 && setTriggerFetch((prev) => !prev)}
-        />
-      </ErrorScreen>
-    );
-  }
+  // if (error) {
+  //   return (
+  //     <ErrorScreen errorMessage={error}>
+  //       <Search
+  //         onFocus={(e) => (e.target.value = "")}
+  //         onChange={(e) => setCityInput(e.target.value)}
+  //         onKeyDown={(e) => e.keyCode === 13 && setTriggerFetch((prev) => !prev)}
+  //       />
+  //     </ErrorScreen>
+  //   );
+  // }
+
+  if (error) return <ErrorScreen errorMessage={error} />;
 
   if (!weatherData) return <LoadingScreen loadingMessage="Loading data..." />;
 
@@ -74,7 +77,7 @@ export const App = () => {
       <ContentBox>
         <Header>
           <DateAndTime weatherData={weatherData} unitSystem={unitSystem} />
-          <Search
+          {/* <Search
             placeHolder="Search a city..."
             value={cityInput}
             onFocus={(e) => {
@@ -86,7 +89,7 @@ export const App = () => {
               if (e.keyCode === 13) setTriggerFetch((prev) => !prev);
               e.target.placeholder = "Search a city...";
             }}
-          />
+          /> */}
         </Header>
         <MetricsBox weatherData={weatherData} unitSystem={unitSystem} />
         <UnitSwitch onClick={changeSystem} unitSystem={unitSystem} />
